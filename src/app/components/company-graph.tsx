d4d8305d85dd8f9a7bc3a13afea983cf381f45ce
Agam More <agam@agam.me>
2024-11-16 23:52:07 -0600

    Fix image config

--- src/app/components/company-graph.tsx
+++ src/app/components/company-graph.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @next/next/no-img-element */
 
 import { useState, useRef, useEffect } from "react";
 import { Plus, ChevronLeft, ChevronRight, Presentation } from "lucide-react";
